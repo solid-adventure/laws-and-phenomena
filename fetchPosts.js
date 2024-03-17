612f5a9a75a1af99f8a399da92c673b0bddb7c97
James Marks <james.marks@withtrivial.com>
2024-03-17 15:21:50 -0700

    build script demo

---
+++ fetchPosts.js
@@ -0,0 +1,25 @@
+
+
+class PostSync {
+  constructor() {
+
+  }
+
+  static test() {
+    console.log('<<<<<<<<<<<<  PostSync Test >>>>>>>>>>>>>')
+  }
+
+  fetchFromNetlify() {
+
+  }
+
+  format() {
+
+  }
+
+  writeLocally() {
+
+  }
+}
+
+PostSync.test()
